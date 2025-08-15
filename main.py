b70f7dd69ad186b61f514955a2efc5b2d4a42bde
Beatriz <beatrizcazatti@gmail.com>
2025-08-15 09:37:36 -0300

    altera graficos

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ import matplotlib.pyplot as plt
 
 amostra = 20
 
-hoje = datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+hoje = datetime.now().strftime("%H:%M:%S")
 periodo = pd.date_range(start=hoje, periods=amostra, freq='min')
 
-#np.random.normal
-temperatura = np.random.normal(loc=15, scale=10, size=amostra).round(2)
-#np.random.uniform
+temperatura = np.random.normal(loc=15, scale=15, size=amostra).round(2)
 luminosidade = np.random.uniform(low=0, high=1000, size=amostra).round(2)
 #0 = sem movimento, 1 = movimento detectado. p=[0.8, 0.2] => 80% das vezes sem movimento, 20% com movimento
 movimento = np.random.choice([0, 1], size=amostra, p=[0.8, 0.2])
@@ -23,8 +21,8 @@ dt = {
 }
 
 data_frame = pd.DataFrame(dt)
-#print(data_frame)
-#pd.DataFrame(data_frame).to_csv(f'registro_{datetime.now().strftime("%d_%m_%y")}', index=False)
+print(data_frame)
+pd.DataFrame(data_frame).to_csv(f'registro_{datetime.now().strftime("%d_%m_%y")}', index=False)
 
 titulo = {'family': 'serif', 'size': '13', 'color': 'black'}
 label = {'family': 'serif', 'size': '11', 'color': 'gray'}
@@ -32,38 +30,27 @@ x = dt["registro"]
 t = dt["temperatura"]
 l = dt["luminosidade"]
 m = dt["movimento"]
-'''
+
+fig, axs = plt.subplots(2, 1, sharex=True) 
+fig.subplots_adjust(hspace=0.1)
+fig.suptitle("Registro dos Sensores")
+
 #Temperatura
-plt.figure(figsize=(13, 8))
-plt.title("Variação de Temperatura", fontdict = titulo, loc='left')
-
-plt.plot(x, t, label='temperatura', color='green')
-plt.xlabel('registros', fontdict=label)
-plt.ylabel('temperatura(°C)', fontdict=label)
-
-plt.grid(axis = 'y', color = 'gray', linestyle = '--', linewidth = 0.7)
-plt.legend()
-plt.show()
-
+axs[0].plot(x, t, 'o--', color='green')
+axs[0].set_ylabel('Temperatura (°C)', fontdict = label, loc='top')
+axs[0].grid(axis = 'x', color = 'gray', linestyle = '--', linewidth = 0.7)
 
 #Luminosidade
-plt.figure(figsize=(13, 8))
-plt.title("Luminosidade", fontdict = titulo, loc='left')
-
-plt.plot(x, l, label='luminosidade', color='orange')
-plt.xlabel('registros', fontdict=label)
-plt.ylabel('luminosidade(lux)', fontdict=label)
-
-plt.grid(axis = 'y', color = 'gray', linestyle = '--', linewidth = 0.7)
-plt.legend()
-plt.show()
-
+axs[1].plot(x, l, 'o--', color='gold')
+axs[1].set_ylabel('Luminosidade (lux)', fontdict = label, loc='bottom')
+axs[1].set_xlabel('Tempo')
+axs[1].grid(axis = 'x', color = 'gray', linestyle = '--', linewidth = 0.7)
 
 #Movimento
-plt.title("Movimento")
-#plt.grid(axis = 'y', color = 'gray', linestyle = '--', linewidth = 0.7)
-#plt.legend()
-plt.hist(m)
-#plt.plot(x, m, label='movimento', color='blue', marker='o')
+plt.figure(figsize=(7, 5))
+plt.title("Sensor de Movimento")
+
+plt.plot(x, m, 'o', color=None)
+plt.grid(axis = 'x', color = 'gray', linestyle = '--', linewidth = 0.7)
 plt.show()
-'''
+
